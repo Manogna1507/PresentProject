6ccda73e45994ad5a3fc6af92ef03b18dc4dee3f
Manogna <manognagunnal15@gmail.com>
2024-11-25 12:18:53 +0530

    Demo for Test

---
+++ src/main/java/MavenJavaProject/PresentProject/App.java
@@ -0,0 +1,14 @@
+package MavenJavaProject.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("22BD1A664Q \n G.Manogna");
+    }
+}
